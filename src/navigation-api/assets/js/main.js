40b29bc3e8266760519ad512a2bd9258a7b3918e
yend724 <yend9524@gmail.com>
2023-08-13 14:40:36 +0900

    Add Navigation API Demo

---
+++ src/navigation-api/assets/js/main.js
@@ -0,0 +1,42 @@
+import 'destyle.css';
+import '../css/style.css';
+
+const parser = new DOMParser();
+const parseHTML = html => {
+  return parser.parseFromString(html, 'text/html');
+};
+const getHTML = async url => {
+  return fetch(url).then(res => res.text());
+};
+const swap = (from, to) => {
+  from.replaceWith(to);
+};
+
+const shouldNotIntercept = navigationEvent => {
+  // 参考: https://developer.chrome.com/docs/web-platform/navigation-api/#deciding-how-to-handle-a-navigation
+  return (
+    !navigationEvent.canIntercept ||
+    navigationEvent.hashChange ||
+    navigationEvent.downloadRequest ||
+    navigationEvent.formData
+  );
+};
+navigation.addEventListener('navigate', e => {
+  if (shouldNotIntercept(e)) return;
+
+  const loadNextPage = async () => {
+    const htmlString = await getHTML(e.destination.url);
+    const parsedHTML = parseHTML(htmlString);
+    const toHTML = parsedHTML.querySelector('*[data-transition-wrapper]');
+    const fromHTML = document.querySelector('*[data-transition-wrapper]');
+    swap(fromHTML, toHTML);
+    document.title = to.title;
+  };
+  e.intercept({ handler: loadNextPage });
+});
+navigation.addEventListener('navigatesuccess', e => {
+  console.log(e);
+});
+navigation.addEventListener('navigateerror', e => {
+  console.error(e);
+});
